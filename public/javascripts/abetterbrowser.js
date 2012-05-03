8fb22ad71b5363e6902eab70771a052c9693a5af
xPaw <thexpaw@gmail.com>
2012-05-03 17:05:34 +0300

    Use CloudFlare's DOM methods.

--- public/javascripts/abetterbrowser.js
+++ public/javascripts/abetterbrowser.js
@@ -11,11 +11,11 @@ CloudFlare.define( 'abetterbrowser', [ 'cloudflare/dom' ], function( dom )
 {
 	if( dom.internetExplorer !== undefined && dom.internetExplorer < 8 )
 	{
-		var cssCode = '#bad_browser { border-bottom: 1px solid #000; background: #45484d; position: absolute; width: 100%; top: 0; left: 0; padding: 8px 0; font-family: Helvetica Neue, Helvetica, Arial, sans-serif; font-size: 18px; text-align: center; color: #FFF; } #bad_browser a { text-decoration: underline; color: #EBEBF4 } #bad_browser a:hover, #bad_browser a:active { text-decoration: none; color: #dbdbeb }';
+		var cssCode = '#bad_browser { border-bottom: 1px solid #000; background: #45484d; position: absolute; width: 100%; top: 0; left: 0; padding: 8px 0; font-family: "Segoe UI", sans-serif; font-size: 18px; text-align: center; color: #FFF; } #bad_browser a { text-decoration: underline; color: #EBEBF4 } #bad_browser a:hover, #bad_browser a:active { text-decoration: none; color: #dbdbeb }';
 		
 		// Create <style> element
-		var style = document.createElement( 'style' );
-		style.setAttribute( 'type', 'text/css' );
+		var style = dom.createElement( 'style' );
+		dom.setAttribute( style, 'type', 'text/css' );
 		
 		if( style.styleSheet )
 		{
@@ -27,7 +27,7 @@ CloudFlare.define( 'abetterbrowser', [ 'cloudflare/dom' ], function( dom )
 		}
 		
 		// Create <div> element
-		var wrapper = document.createElement( 'div' );
+		var wrapper = dom.createElement( 'div' );
 		wrapper.id = 'bad_browser';
 		wrapper.innerHTML = 'You are using an outdated browser. <a href="http://abetterbrowser.org/">More information &#187;</a>';
 		
