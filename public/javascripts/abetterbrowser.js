1095b09c08d10db5cd98585506a1fbad1354cbd0
xPaw <me@xpaw.ru>
2012-05-26 00:19:48 +0300

    Add z-index
    Some website have z-index set, thus bar shows under some elements and
    becomes broken.

--- public/javascripts/abetterbrowser.js
+++ public/javascripts/abetterbrowser.js
@@ -11,7 +11,7 @@ CloudFlare.define( 'abetterbrowser', [ 'cloudflare/dom' ], function( dom )
 {
 	if( dom.internetExplorer !== undefined && dom.internetExplorer < 8 )
 	{
-		var cssCode = '#bad_browser { border-bottom: 1px solid #000; background: #45484d; position: absolute; width: 100%; top: 0; left: 0; padding: 8px 0; font-family: "Segoe UI", sans-serif; font-size: 18px; text-align: center; color: #FFF; } #bad_browser a { text-decoration: underline; color: #EBEBF4 } #bad_browser a:hover, #bad_browser a:active { text-decoration: none; color: #dbdbeb }';
+		var cssCode = '#bad_browser { border-bottom: 1px solid #000; background: #45484d; position: absolute; z-index: 10000; width: 100%; top: 0; left: 0; padding: 8px 0; font-family: "Segoe UI", sans-serif; font-size: 18px; text-align: center; color: #FFF; } #bad_browser a { text-decoration: underline; color: #EBEBF4 } #bad_browser a:hover, #bad_browser a:active { text-decoration: none; color: #dbdbeb }';
 		
 		// Create <style> element
 		var style = dom.createElement( 'style' );
