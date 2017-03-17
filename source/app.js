a57c6ec3858996669ad02629ad384cd58859d189
Teffen Ellis (Eric) <teffen@nirri.us>
2017-03-17 13:17:54 -0700

    Add legacy IDs.

--- source/app.js
+++ source/app.js
@@ -5,6 +5,7 @@ import translations from './translations'
   let options = INSTALL_OPTIONS
   let appElement
   const {localStorage = {}} = window
+  const {body} = document
   const DAY_DURATION = 1000 * 60 * 60 * 24
   const now = new Date()
   const weekAgo = new Date(now - DAY_DURATION * 7)
@@ -13,10 +14,17 @@ import translations from './translations'
   const browserVersion = parseFloat(browser.version.match(/(\d.)\./))
   const browserMinimum = options[browser.name] || 0
 
+  const legacyBodyClass = 'cloudflare-old-browser-body'
+
+  function removeBodyClass () {
+    body.className = body.className.replace(new RegExp(`(?:^|s)${legacyBodyClass}(?!S)`, 'g'), '')
+  }
+
   function updateElement () {
     const outdated = browserVersion < browserMinimum
     let visibility = !seenRecently && outdated ? 'visible' : 'hidden'
 
+    removeBodyClass()
     if (INSTALL_ID === 'preview') visibility = 'visible'
     if (visibility !== 'visible') return
 
@@ -25,6 +33,7 @@ import translations from './translations'
 
     appElement = appElement || document.createElement('cloudflare-app')
     appElement.setAttribute('app-id', 'a-better-browser')
+    appElement.id = 'cloudflare-old-browser' // Legacy ID
 
     appElement.innerHTML = `
       <cloudflare-app-message>
@@ -34,18 +43,21 @@ import translations from './translations'
       <cloudflare-app-close>&times;</cloudflare-app-close>
     `
     const closeButton = appElement.querySelector('cloudflare-app-close')
+    closeButton.id = 'cloudflare-old-browser-close' // Legacy ID
 
     closeButton.addEventListener('click', () => {
       appElement.setAttribute('data-visibility', 'hidden')
       localStorage.cfBetterBrowserDismissedAt = now.getTime()
+      removeBodyClass()
     })
 
     appElement.setAttribute('data-visibility', visibility)
-    document.body.appendChild(appElement)
+    body.appendChild(appElement)
 
-    document.body.setAttribute('data-cf-browser-state', outdated ? 'outdated' : 'modern')
-    document.body.setAttribute('data-cf-browser-version', browserVersion)
-    document.body.setAttribute('data-cf-browser-name', browser.name)
+    body.className += legacyBodyClass
+    body.setAttribute('data-cf-browser-state', outdated ? 'outdated' : 'modern')
+    body.setAttribute('data-cf-browser-version', browserVersion)
+    body.setAttribute('data-cf-browser-name', browser.name)
   }
 
   if (document.readyState === 'loading') {
